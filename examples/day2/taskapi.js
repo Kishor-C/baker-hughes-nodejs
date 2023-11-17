befabe2e3e1093ae6da01176b09fb60d7da21d00
Kishor <kishor1124@gmail.com>
2023-11-17 12:53:10 +0530

    express server code

---
+++ examples/day2/taskapi.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import { addTask, listTask } from './task-manager';
+let app = express(); // create a top-level express object
+app.use(express.json()); // extracts json from request
+app.use(cors()); // to enable cross-origin-resource-share
+// HTTP GET
+app.get('/task', (request, response) => {
+    let allTasks = listTask();
+    response.status(200).json(allTasks);
+});
+// HTTP POST
+app.post('/task', (request, response) => {
+    let body = request.body; // retruns data in req body
+    let status = addTask(body);
+    status ? 
+        response.status(201).json({"message":"Stored!"})
+        : response.status(400).json({"error":"Not stored"})
+});
+app.listen(8888, () => console.log('Running in 8888'));
