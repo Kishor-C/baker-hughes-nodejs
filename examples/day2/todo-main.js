12977be037f4c1315b91064b56ba50b35343538c
Kishor <kishor1124@gmail.com>
2023-11-17 11:43:28 +0530

    todo tasks add and list

--- examples/day2/todo-main.js
+++ examples/day2/todo-main.js
@@ -1,9 +1,20 @@
 import readline from 'readline-sync';
-import { listTask } from './task-manager.js';
+import { addTask, listTask } from './task-manager.js';
 let option = 0;
 do { 
     console.log(`****Todo**** \n 1: Add 2: List -1: Exit`)
     option = readline.questionInt('Enter your option: ');
+    if(option == 1) {
+        let task = {
+            id: readline.questionInt('Enter id: '),
+            date: readline.question('Enter date: '),
+            task: readline.question('Enter task: ')
+        }
+        let status = addTask(task);
+        if(status) {
+            console.log('Task is added..');
+        }
+    }
     if(option == 2) {
         console.log(listTask());
     }
